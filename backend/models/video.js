ce0b07780907dccb2c45183d5ae5077863614224
dwu006 <happy4danielwu@gmail.com>
2025-05-11 16:58:13 -0700

    Class Controller

--- backend/models/video.js
+++ backend/models/video.js
@@ -8,7 +8,7 @@ const videoSchema = new mongoose.Schema({
   link: { type: String },
   transcript: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+  className: { type: String },
 }, { timestamps: true });
 
 export default mongoose.model('Video', videoSchema);
