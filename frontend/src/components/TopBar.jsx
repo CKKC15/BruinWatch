2cd7a22d2f1b0cab33082a636e38c6bd07dbd9c5
s0phiachang <sclairechang@gmail.com>
2025-05-11 14:00:52 -0700

    Update TopBar.jsx

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,1 +1,22 @@
+import React from 'react':
+import {FaSearch} from 'react-icons/fa';
 
+export default function TopBar(props) {
+  function handleInputChange(event) {
+    const userInput = event.target.value;
+    props.onSearch(userInput);
+  }
+  return(
+    <div className="topbar-wrapper">
+      <div className = "topbar-search">
+        <FaSearch className ="topbar-icon" />
+        <input
+          type = "text"
+          placeholder="Search lectures..."
+          classname="topbar-input"
+          onChange={handleInputChange}
+          />
+        </div>
+      </div>
+    );
+}
